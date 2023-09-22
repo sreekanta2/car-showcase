4e8266be4cae42bb9a0f63154affcc922d585e8d
sreekanta2 <sreekanta03s@gmail.com>
2023-09-22 11:17:16 +0600

    first commit

---
+++ components/index.tsx
@@ -0,0 +1,20 @@
+import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import SearchBar from "./SearchBar";
+import ShowMore from "./ShowMore";
+
+import Hero from "./Hero";
+
+export {
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  Hero,
+  NavBar,
+  SearchBar,
+  ShowMore,
+};
